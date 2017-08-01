ffac00ee06f202e196b9f1403f0c62054a349f7e
Tyler Negro <TylerNegro@Tylers-MBP.lan>
2017-07-31 19:18:40 -0600

    db connect started

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -10,26 +10,53 @@ $(document).ready(function(){
   firebase.initializeApp(config);
 
   var database = firebase.database();
-  var user = $('#username-input');
-  var userValue; 
+  var userObject = {
+    name:'',
+    wins:0,
+    losses:0,
+    choice:'',
+  }
+
+
+var userListRef = database.ref("users_online");
+var myUserRef = userListRef.push()
+
+//this is setting the status from the user object to the db
+function setUserStatus(object){
+  myUserRef.set(object)
+}
+
+//this is looking to see if client connected or disconnected
+database.ref(".info/connected").on("value", function(snapshot){
+  if(snapshot.val()){
+    myUserRef.onDisconnect().remove();
+    // setUserStatus("online");
+  }
+})
+
+function rps(){
+  userObject.choice = 'rock'
+  setUserStatus(userObject)
+}
 
 
 
-  //gets username input and will store it to the DB
-  
+  // gets username input and will store it to the DB
   function usernameRetrieval(){
     $('#user-submit').on('click', function(event){
       event.preventDefault();
-      userValue  = user[0].value
-      console.log(userValue)
+      userObject.name = $('#username-input').val().trim();
+      setUserStatus(userObject)
       $('.player-info').addClass('hide');
       $('.main').removeClass('hidden');
     })
-  }
+  }  
+
  
 
   function main(){
     usernameRetrieval();
+    rps();
  }
 
  main()
